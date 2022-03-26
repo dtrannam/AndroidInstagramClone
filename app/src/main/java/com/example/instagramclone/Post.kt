ab3688685a16649cff723ff9c78d4b431b1758cd
David Trannam <trannamd@oregonstate.edu>
2022-03-25 23:56:16 -0700

    Part 1

---
+++ app/src/main/java/com/example/instagramclone/Post.kt
@@ -0,0 +1,39 @@
+package com.example.instagramclone
+
+import com.parse.ParseClassName
+import com.parse.ParseFile
+import com.parse.ParseObject
+import com.parse.ParseUser
+
+@ParseClassName("Post")
+class Post : ParseObject() {
+    companion object {
+        const val KEY_DESCRIPTION = "description"
+        const val KEY_IMAGE = "image"
+        const val KEY_USER = "user"
+    }
+
+    fun getDescription(): String? {
+        return getString(KEY_DESCRIPTION)
+    }
+
+    fun getImage(): ParseFile? {
+        return getParseFile(KEY_IMAGE)
+    }
+
+    fun getUser(): ParseUser? {
+        return getParseUser(KEY_USER)
+    }
+
+    fun setDescription(description: String) {
+        put(KEY_DESCRIPTION, description)
+    }
+
+    fun setImage(parseFile: ParseFile) {
+        put(KEY_IMAGE, parseFile)
+    }
+
+    fun setUser(user: ParseUser) {
+        put(KEY_USER, user)
+    }
+}
